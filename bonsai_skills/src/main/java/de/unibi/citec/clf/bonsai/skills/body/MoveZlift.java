95288b06c3cfdf35406c615d80a96486229f740a
Markus Vieth <mvieth@techfak.uni-bielefeld.de>
2019-09-27 15:51:17 +0200

    Add functionality to specify move duration in move z lift

--- bonsai_skills/src/main/java/de/unibi/citec/clf/bonsai/skills/body/MoveZlift.java
+++ bonsai_skills/src/main/java/de/unibi/citec/clf/bonsai/skills/body/MoveZlift.java
@@ -16,7 +16,32 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * change the zlift position.
+ * Change the zlift position.
+ *
+ * <pre>
+ *
+ * Options:
+ *  #_POSITION:      [Double] Optional (Default: 0)
+ *                      -> Z lift position, range depending on the robot (Tiago: 0.0-0.35)
+ *  #_MOVE_DURATION: [Integer] Optional (Default: 4000)
+ *                      -> Time the lift takes to move to the position in milliseconds
+ *  #_TIMEOUT:       [Integer] Optional (default: 7000)
+ *                      -> Amount of time robot waits for actuator to be done in milliseconds
+ *  #_SLOT:          [boolean] Optional (default: false)
+ *                      -> If true the position is read from a slot
+ *
+ * Slots:
+ *
+ * ExitTokens:
+ *  success:    Head movement completed successfully
+ *  error
+ *
+ * Sensors:
+ *
+ * Actuators:
+ *  ZliftActuator
+ *
+ * </pre>
  *
  * TODO: unmekafy
  * 
@@ -25,6 +50,7 @@ import java.util.logging.Logger;
 public class MoveZlift extends AbstractSkill {
 
     private static final String KEY_POSITION = "#_POSITION";
+    private static final String KEY_MOVE_DURATION = "#_MOVE_DURATION";
     private static final String KEY_TIMEOUT = "#_TIMEOUT";
     private static final String KEY_SLOT = "#_SLOT";
 
@@ -34,8 +60,9 @@ public class MoveZlift extends AbstractSkill {
     private ExitToken tokenError;
 
     private float pos;
+    private int move_duration = 4000;
     private long timeout = 7000;
-    Future<Boolean> b;
+    private Future<Boolean> b;
 
     private MemorySlotReader<Double> heightSlot;
     private Boolean readHeightSlot;
@@ -48,6 +75,7 @@ public class MoveZlift extends AbstractSkill {
         tokenSuccess = configurator.requestExitToken(ExitStatus.SUCCESS());
         tokenError = configurator.requestExitToken(ExitStatus.ERROR());
 
+        move_duration = configurator.requestOptionalInt(KEY_MOVE_DURATION, move_duration);
         timeout = configurator.requestOptionalInt(KEY_TIMEOUT, (int) timeout);
         readHeightSlot = configurator.requestOptionalBool(KEY_SLOT, false);
 
@@ -73,7 +101,7 @@ public class MoveZlift extends AbstractSkill {
         }
 
         try {
-            b = jointcontroller.moveTo(pos, null);
+            b = jointcontroller.moveTo(pos, 1000.0f/move_duration);
         } catch (IOException ex) {
             logger.error(ex);
             return false;
