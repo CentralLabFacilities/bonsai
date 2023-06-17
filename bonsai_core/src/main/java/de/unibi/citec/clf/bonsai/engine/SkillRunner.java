dccd21fd2dc547d1bffda208e4404f39f47b35c3
rc@thorium <robocup-adm@thorium>
2023-06-17 15:50:53 +0200

    engine: dont wait endlessly for notify

--- bonsai_core/src/main/java/de/unibi/citec/clf/bonsai/engine/SkillRunner.java
+++ bonsai_core/src/main/java/de/unibi/citec/clf/bonsai/engine/SkillRunner.java
@@ -361,7 +361,7 @@ public class SkillRunner implements Runnable {
             }
 
             logger.debug("waiting for skill to quit");
-            wait();
+            wait(1000);
 
         } catch (InterruptedException ex) {
             Thread.currentThread().interrupt();
