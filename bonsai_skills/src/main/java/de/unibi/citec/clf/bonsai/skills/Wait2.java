3e6ec7913533ddc7ad4940cdc1d6a4495ee1108b
jvaquet <jvaquet@techfak.de>
2023-06-19 12:58:27 +0200

    Added Wait2

---
+++ bonsai_skills/src/main/java/de/unibi/citec/clf/bonsai/skills/Wait2.java
@@ -0,0 +1,69 @@
+package de.unibi.citec.clf.bonsai.skills;
+
+import de.unibi.citec.clf.bonsai.core.time.Time;
+import de.unibi.citec.clf.bonsai.engine.model.AbstractSkill;
+import de.unibi.citec.clf.bonsai.engine.model.ExitStatus;
+import de.unibi.citec.clf.bonsai.engine.model.ExitToken;
+import de.unibi.citec.clf.bonsai.engine.model.config.ISkillConfigurator;
+
+/**
+ * Identical Copy of Wait; however we can use this if we need to wait twice in a parallel state :)
+ * Wait for a specified amount of time.
+ *
+ * <pre>
+ *
+ * Options:
+ *  #_TIMEOUT:  [double] Required 
+ *                  -> Time to wait in ms. Note that times will effectively be rounded up to half second intervals
+ *
+ * Slots:
+ *
+ * ExitTokens:
+ *  success:    Specified time elapsed
+ *
+ * Sensors:
+ *
+ * Actuators:
+ *
+ * </pre>
+ *
+ * @author lkettenb
+ * @author jkummert
+ */
+public class Wait2 extends AbstractSkill {
+
+    private static final String DEFAULT_KEY = "#_TIMEOUT";
+    private long timeout = 1000;
+
+    private ExitToken tokenSuccess;
+
+    @Override
+    public void configure(ISkillConfigurator configurator) {
+
+        timeout = configurator.requestOptionalInt(DEFAULT_KEY, (int)timeout);
+        
+        tokenSuccess = configurator.requestExitToken(ExitStatus.SUCCESS());
+    }
+
+    @Override
+    public boolean init() {
+
+        logger.debug("waiting for " + timeout + "ms");
+        timeout = Time.currentTimeMillis() + timeout;
+
+        return true;
+    }
+
+    @Override
+    public ExitToken execute() {
+        if (timeout < Time.currentTimeMillis()) {
+            return tokenSuccess;
+        }
+        return ExitToken.loop(50);
+    }
+
+    @Override
+    public ExitToken end(ExitToken curToken) {
+        return curToken;
+    }
+}
