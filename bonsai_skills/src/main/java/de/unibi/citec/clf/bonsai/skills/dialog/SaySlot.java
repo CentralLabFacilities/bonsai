04c574cd2987ea9a29600987fb9ea1fa9434bfd9
nschmitz <niklas.schmitz@uni-bielefeld.de>
2025-02-26 18:27:34 +0100

    Added multilanguage support to sayslot

--- bonsai_skills/src/main/java/de/unibi/citec/clf/bonsai/skills/dialog/SaySlot.java
+++ bonsai_skills/src/main/java/de/unibi/citec/clf/bonsai/skills/dialog/SaySlot.java
@@ -7,6 +7,8 @@ import de.unibi.citec.clf.bonsai.engine.model.AbstractSkill;
 import de.unibi.citec.clf.bonsai.engine.model.ExitStatus;
 import de.unibi.citec.clf.bonsai.engine.model.ExitToken;
 import de.unibi.citec.clf.bonsai.engine.model.config.ISkillConfigurator;
+import de.unibi.citec.clf.btl.data.speechrec.Language;
+import de.unibi.citec.clf.btl.data.speechrec.LanguageType;
 
 import java.io.IOException;
 import java.util.concurrent.Future;
@@ -39,15 +41,15 @@ import java.util.regex.Matcher;
  *
  * </pre>
  *
- *
  * @author rfeldhans
  * @author jkummert
- *
  */
 public class SaySlot extends AbstractSkill {
 
     private static final String SAY_TEXT = "#_MESSAGE";
     private static final String KEY_BLOCKING = "#_BLOCKING";
+
+    private static final String KEY_USE_LANGUAGE = "#_USE_LANGUAGE";
     private static final String REPLACE_STRING = "$S";
 
     private boolean blocking = true;
@@ -57,9 +59,11 @@ public class SaySlot extends AbstractSkill {
 
     private MemorySlotReader<String> stringSlot;
 
+    private MemorySlotReader<LanguageType> langSlot = null;
+
     private SpeechActuator speechActuator;
 
-    private Future<Void> sayingComplete;
+    private Future<String> sayingComplete;
 
     @Override
     public void configure(ISkillConfigurator configurator) {
@@ -69,10 +73,24 @@ public class SaySlot extends AbstractSkill {
         tokenSuccess = configurator.requestExitToken(ExitStatus.SUCCESS());
         speechActuator = configurator.getActuator("SpeechActuator", SpeechActuator.class);
         stringSlot = configurator.getReadSlot("StringSlot", String.class);
+
+        if (configurator.requestOptionalBool(KEY_USE_LANGUAGE, false)) {
+            langSlot = configurator.getReadSlot("Language", LanguageType.class);
+        }
     }
 
     @Override
     public boolean init() {
+        Language lang = Language.EN;
+        try {
+            if (langSlot != null) {
+                lang = langSlot.recall().getValue();
+            }
+        } catch (CommunicationException ex) {
+            logger.error("Could not read from language slot", ex);
+            return false;
+        }
+
 
         String sayStr = null;
         try {
@@ -93,7 +111,7 @@ public class SaySlot extends AbstractSkill {
         logger.info("saying: " + sayStr);
 
         try {
-            sayingComplete = speechActuator.sayAsync(sayStr);
+            sayingComplete = speechActuator.sayTranslated(sayStr, lang);
         } catch (IOException e) {
             logger.error("Could not call speech actuator");
             return false;
