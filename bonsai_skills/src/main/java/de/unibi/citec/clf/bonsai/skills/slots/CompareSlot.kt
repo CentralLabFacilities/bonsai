bdcfe2c02c619b7c0705947a18dd7b696372f4a0
nschmitz <niklas.schmitz@uni-bielefeld.de>
2025-09-16 12:02:57 +0200

    Reworked CompareSlot skill such that regex is used for comparing. Added corresponding test too.

--- bonsai_skills/src/main/java/de/unibi/citec/clf/bonsai/skills/slots/CompareSlot.kt
+++ bonsai_skills/src/main/java/de/unibi/citec/clf/bonsai/skills/slots/CompareSlot.kt
@@ -1,83 +1,93 @@
-package de.unibi.citec.clf.bonsai.skills.slots;
+package de.unibi.citec.clf.bonsai.skills.slots
 
-import de.unibi.citec.clf.bonsai.core.exception.CommunicationException;
-import de.unibi.citec.clf.bonsai.core.object.MemorySlotReader;
-import de.unibi.citec.clf.bonsai.engine.model.AbstractSkill;
-import de.unibi.citec.clf.bonsai.engine.model.ExitStatus;
-import de.unibi.citec.clf.bonsai.engine.model.ExitToken;
-import de.unibi.citec.clf.bonsai.engine.model.config.ISkillConfigurator;
+import de.unibi.citec.clf.bonsai.core.exception.CommunicationException
+import de.unibi.citec.clf.bonsai.core.`object`.MemorySlotReader
+import de.unibi.citec.clf.bonsai.engine.model.AbstractSkill
+import de.unibi.citec.clf.bonsai.engine.model.ExitStatus
+import de.unibi.citec.clf.bonsai.engine.model.ExitToken
+import de.unibi.citec.clf.bonsai.engine.model.config.ISkillConfigurator
 
 /**
- * Read from ReadSlot and compare to compareString.
+ * Read from ReadSlot and compare to given regex. String read from slot is getting formated to lower case before
+ * comparing.
  *
  * <pre>
  *
+ *
+ * Options:
+ * #_COMPARE_STRING:         [String]
+ *  -> Regex used for comparing
+ *
  * Slots:
- *  ReadSlot: [String] [Read]
- *      -> Memory slot the content will be read from
+ * ReadSlot: [String] [Read]
+ *  -> Memory slot the content will be read from
+ *
+ *
  *
  * ExitTokens:
- *  success.match:      Entries are the same
- *  success.mismatch:   Entries are not the same
- *  error:              ReadSlot was empty
+ * success.match:      Regex matched
+ * success.mismatch:   Regex didn't match
+ * error:              ReadSlot was empty
  *
  * Sensors:
  *
  * Actuators:
  *
- * </pre>
+</pre> *
  *
- * @author pvonneumanncosel
+ * @author nschmitz
  */
-public class CompareSlot extends AbstractSkill {
+class CompareSlot : AbstractSkill() {
+    private lateinit var tokenMisMatch: ExitToken
+    private lateinit var tokenMatch: ExitToken
 
-    private ExitToken tokenMisMatch;
-    private ExitToken tokenMatch;
+    private var readSlot: MemorySlotReader<String?>? = null
 
-    private static final String KEY_COMPARE_STRING = "#_COMPARE_STRING";
+    private var slotContent: String? = null
+    private var comparePattern: String? = null
 
-    MemorySlotReader<String> readSlot;
-
-    private String slotContent;
-    private String compareString;
-
-    @Override
-    public void configure(ISkillConfigurator configurator) {
-        tokenMatch = configurator.requestExitToken(ExitStatus.SUCCESS().withProcessingStatus("match"));
-        tokenMisMatch = configurator.requestExitToken(ExitStatus.SUCCESS().withProcessingStatus("misMatch"));
+    override fun configure(configurator: ISkillConfigurator) {
+        tokenMatch = configurator.requestExitToken(ExitStatus.SUCCESS().withProcessingStatus("match"))
+        tokenMisMatch = configurator.requestExitToken(ExitStatus.SUCCESS().withProcessingStatus("misMatch"))
 
 
-        readSlot = configurator.getReadSlot("ReadSlot", String.class);
-        compareString = configurator.requestValue(KEY_COMPARE_STRING);
+        readSlot = configurator.getReadSlot("ReadSlot", String::class.java)
+        comparePattern = configurator.requestValue(KEY_COMPARE_STRING)
     }
 
-    @Override
-    public boolean init() {
+    override fun init(): Boolean {
         try {
-            slotContent = readSlot.recall();
+            slotContent = readSlot?.recall<String?>()
 
             if (slotContent == null) {
-                logger.warn("your ReadSlot was empty");
-                return false;
+                logger.warn("your ReadSlot was empty")
+                return false
             }
-
-        } catch (CommunicationException ex) {
-            logger.fatal("Unable to read from memory: ", ex);
-            return false;
+        } catch (ex: CommunicationException) {
+            logger.fatal("Unable to read from memory: ", ex)
+            return false
         }
-        return true;
+        if (comparePattern == "'" || comparePattern!!.isEmpty() || comparePattern!!.isBlank()) {
+            logger.warn("Your Regex Pattern is empty. Defaulting to match.")
+        }
+        return true
     }
 
-    @Override
-    public ExitToken execute() {
-        if (slotContent.equals(compareString)) {
-            return tokenMatch;
+    override fun execute(): ExitToken {
+        val sanitizedSlotContent = slotContent?.lowercase() ?: "unknown"
+        logger.debug("Trying to match regex '$comparePattern' with slot-content '$sanitizedSlotContent'")
+        if (comparePattern?.toRegex()?.matches(sanitizedSlotContent) ?: true) {
+            return tokenMatch
         }
-        return tokenMisMatch;
+        return tokenMisMatch
     }
 
-    @Override
-    public ExitToken end(ExitToken curToken) {
-        return curToken;
+    override fun end(curToken: ExitToken?): ExitToken? {
+        return curToken
+    }
+
+    companion object {
+        // Name of option could be better, its terrible right now
+        private const val KEY_COMPARE_STRING = "#_COMPARE_STRING"
     }
 }
