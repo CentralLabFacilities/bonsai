c797d766419622360a6f999c9b66526d13378b63
Leroy Rügemer <lruegeme@techfak.uni-bielefeld.de>
2026-01-26 15:03:07 +0100

    llm updates

--- bonsai_interfaces/src/main/java/de/unibi/citec/clf/btl/data/speech/llm/Message.kt
+++ bonsai_interfaces/src/main/java/de/unibi/citec/clf/btl/data/speech/llm/Message.kt
@@ -2,12 +2,20 @@ package de.unibi.citec.clf.btl.data.speech.llm
 
 import de.unibi.citec.clf.btl.StampedType
 
-enum class Role {
-    SYSTEM,
-    USER,
-    AGENT,
-    TOOL_CALL,
-    TOOL_RESULT,
+enum class Role(val code: Int) {
+    SYSTEM(0),
+    USER(1),
+    AGENT(2),
+    TOOL_CALL(3),
+    TOOL_RESULT(4);
+
+    fun toInt() = code
+
+    companion object {
+        private val codeToStatus = entries.associateBy { it.code }
+        fun fromCode(code: Int): Role? = codeToStatus[code]
+        fun fromCodeStrict(code: Int): Role = codeToStatus[code] ?: throw IllegalArgumentException("Unknown status code: $code")
+    }
 }
 
 open class Message(var role: Role, var content: String) : StampedType(), Cloneable {
