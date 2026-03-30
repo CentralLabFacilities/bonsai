3cf21c530beabe0fb8d715814b6cc41715a8451e
Leroy Ruegemer <lruegeme@techfak.uni-bielefeld.de>
2026-03-30 16:23:11 +0200

    interface: StorageArea isInside fun

--- bonsai_interfaces/src/main/java/de/unibi/citec/clf/btl/data/ecwm/StorageArea.kt
+++ bonsai_interfaces/src/main/java/de/unibi/citec/clf/btl/data/ecwm/StorageArea.kt
@@ -1,7 +1,10 @@
 package de.unibi.citec.clf.btl.data.ecwm
 
 import de.unibi.citec.clf.btl.StampedType
+import de.unibi.citec.clf.btl.data.geometry.Pose3D
+import de.unibi.citec.clf.btl.units.LengthUnit
 import java.util.*
+import kotlin.math.abs
 
 open class StorageArea(val name: String, val sizeX: Double, val sizeY: Double, val sizeZ: Double) : StampedType(), Cloneable{
 
@@ -18,6 +21,12 @@ open class StorageArea(val name: String, val sizeX: Double, val sizeY: Double, v
 
     override fun equals(other: Any?): Boolean {
         return super.equals(other) && other is StorageArea && other.name == this.name
+    }
+
+    fun isInside(pose3D: Pose3D): Boolean {
+        return abs(pose3D.translation.getX(LengthUnit.METER)) < sizeX &&
+                abs(pose3D.translation.getY(LengthUnit.METER)) < sizeY &&
+                abs(pose3D.translation.getZ(LengthUnit.METER)) < sizeZ
     }
 
     fun copy(): StorageArea = StorageArea(name, sizeX, sizeY, sizeZ)
