09df49dac62f42beafe88554b74cdef664a3dd27
Leroy Rügemer <lruegeme@techfak.uni-bielefeld.de>
2026-09-02 09:51:33 +0200

    version bumps

--- bonsai_core/src/test/java/de/unibi/citec/clf/bonsai/engine/SkillConfiguratorTest.kt
+++ bonsai_core/src/test/java/de/unibi/citec/clf/bonsai/engine/SkillConfiguratorTest.kt
@@ -5,7 +5,6 @@ import de.unibi.citec.clf.bonsai.skills.ParameterSkill
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
-import java.util.*
 
 class SkillConfiguratorTest {
 
